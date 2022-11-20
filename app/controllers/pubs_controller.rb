010b63c703cdd4adde1222d259c3cbd15b2222b1
Sho Fujiwara <shofujiwara505@gmail.com>
2022-11-20 15:47:44 +0900

    finished my app, time to design

--- app/controllers/pubs_controller.rb
+++ app/controllers/pubs_controller.rb
@@ -3,4 +3,23 @@ class PubsController < ApplicationController
     # able to make a pub
     @pubs = Pub.all
   end
+
+  def new
+    @pubs = Pub.new
+  end
+
+  def create
+    @pubs = Pub.new(pub_params)
+    if @pubs.save
+      redirect_to pubs_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def pub_params
+    params.require(:pub).permit(:name, :location, :rating)
+  end
 end
--- app/controllers/regulars_controller.rb
+++ app/controllers/regulars_controller.rb
@@ -5,7 +5,6 @@ class RegularsController < ApplicationController
 
   def show
     @regular = Regular.find(params[:id])
-
   end
 
   def new
@@ -24,12 +23,28 @@ class RegularsController < ApplicationController
   def destroy
     @regular = Regular.find(params[:id])
     @regular.destroy
-    redirect_to regular_path
+  end
+
+  def edit
+    @regular = Regular.find(params[:id])
+  end
+
+  def update
+    @regular = Regular.find(params[:id])
+    if @regular.update(regular_params)
+      redirect_to regular_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
 
   def regular_params
     params.require(:regular).permit(:name, :favorite_drink, :photo)
+  end
+
+  def regular_find
+
   end
 end
